747aaed96d32892f6b1826187042f97b4dc32380
unknown <zlinus.zachrison@gmail.com>
2021-04-18 15:40:16 +0200

    fetching for register and login

--- script.js
+++ script.js
@@ -5,16 +5,62 @@ const loginTemplate = () => {
   return template;
 };
 
+document.addEventListener("click", (event) => {});
+
+const registerTemplate = () => {
+  template = `<div><h4>Username:</h4> <input type="text" placeholder="Username" id="registerNameInput"> <br> <h4>E-mail: (must contain a valid E-mail)</h4> <input type="text" placeholder="E-mail" id="registerEmailInput"> <br> <h4>Password: (must contain minimum of 6 characters)</h4> <input type="password" placeholder="Password" id="registerPasswordInput"> <br> <div class="checkbox-text">Tick this box to get our amazing newsletter!</div> <input type="checkbox" id="newsletterCheckbox"> <br> <button id="registerBtn">Submit</button></div>`;
+  return template;
+};
+
 document.addEventListener("click", (event) => {
   if (event.target && event.target.id === "registerLink") {
     render(registerTemplate);
   }
-});
+  if (event.target && event.target.id === "registerBtn") {
+    console.log(registerNameInput.value);
+    console.log(registerEmailInput.value);
+    console.log(registerPasswordInput.value);
+    console.log(newsletterCheckbox.checked);
 
-const registerTemplate = () => {
-  template = `<h4>Username:</h4> <input type="text" placeholder="Username" id="registerNameInput"> <br> <h4>E-mail: (must contain a valid E-mail)</h4> <input type="text" placeholder="E-mail" id="registerEmailInput"> <br> <h4>Password: (must contain minimum of 6 characters)</h4> <input type="password" placeholder="Password" id="registerPasswordInput"> <br> <button id="submitBtn">Submit</button>`;
-  return template;
-};
+    let newUser = {
+      name: registerNameInput.value,
+      email: registerEmailInput.value,
+      password: registerPasswordInput.value,
+      newsletter: newsletterCheckbox.checked,
+    };
+
+    fetch(`http://localhost:3000/api/user/register`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  if (event.target && event.target.id === "loginBtn") {
+    let existingUser = {
+      mail: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    fetch(`http://localhost:3000/api/user/login`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(existingUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+});
 
 let render = (template) => {
   root.innerHTML = template();
